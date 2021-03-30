4c54bbd3bb7c7f001d854739dc399af12b2355f4
mehdizit <zitounimehdi.it@gmail.com>
2021-03-30 14:37:52 +0100

    up

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,7 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
-from models.engine.file_storage import storage
+
 
 
 class BaseModel:
@@ -10,6 +10,7 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
+            from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
@@ -29,6 +30,7 @@ class BaseModel:
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
+        from models import storage
         self.updated_at = datetime.now()
         storage.save()
 
