38a9354953a1727781aa460e38e1d9a390732b40
mehdizit <zitounimehdi.it@gmail.com>
2021-03-30 14:28:44 +0100

    up

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,7 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
-from models import storage
+from models.engine.file_storage import storage
 
 
 class BaseModel:
