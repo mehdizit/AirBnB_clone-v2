2585763f593f3ac46c91dfe9bd062a7bf48d42c5
mehdizit <zitounimehdi.it@gmail.com>
2021-03-30 22:02:02 +0100

    up

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,7 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
-from models import storage
+import models
 
 
 class BaseModel:
@@ -14,7 +14,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            models.storage.new(self)
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
@@ -32,7 +32,7 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
         """Convert instance into dict format"""
