28506154364c43b585a99ca4c3ea351606938cd3
mehdizit <zitounimehdi.it@gmail.com>
2021-03-31 19:55:31 +0100

    up

--- models/amenity.py
+++ models/amenity.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 
-class Amenity(BaseModel):
+
+class Amenity(BaseModel, Base):
     name = ""
+
