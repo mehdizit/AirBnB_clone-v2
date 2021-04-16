696d1ecae3d2a5f0954c0f2b156a96b43b2fa2ae
mehdizit <zitounimehdi.it@gmail.com>
2021-04-16 01:39:02 +0100

    up

---
+++ 1-pack_web_static.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Fabric script that generates a .tgz"""
+
+from fabric.api import local
+import time
+
+def do_pack():
+    """creates .tgz archive"""
+    timestr = time.strftime("%Y%m%d%H%M")
+    try:
+        local("mkdir -p versions")
+        local("tar -czvf versions/web_static_{}.tgz web_static/".format(timestr))
+        return ("versions/web_static_{}.tgz".format(timestr))
+    except:
+        return None
