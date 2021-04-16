3fd4c05b5a1b13bf93e077fff4b56b6792e1b656
mehdizit <zitounimehdi.it@gmail.com>
2021-04-16 01:44:38 +0100

    up

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,12 +4,14 @@
 from fabric.api import local
 import time
 
+
 def do_pack():
     """creates .tgz archive"""
     timestr = time.strftime("%Y%m%d%H%M")
     try:
         local("mkdir -p versions")
-        local("tar -czvf versions/web_static_{}.tgz web_static/".format(timestr))
+        local("tar -czvf versions/web_static_{}.tgz web_static/"
+              .format(timestr))
         return ("versions/web_static_{}.tgz".format(timestr))
     except:
         return None
