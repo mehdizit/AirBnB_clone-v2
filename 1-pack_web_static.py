d7e6b0644e38646a773b293f1e0717724f44fdcc
mehdizit <zitounimehdi.it@gmail.com>
2021-04-17 22:42:06 +0100

    up

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,7 +7,7 @@ import time
 
 def do_pack():
     """creates .tgz archive"""
-    timestr = time.strftime("%Y%m%d%H%M")
+    timestr = time.strftime("%Y%m%d%H%M%S")
     try:
         local("mkdir -p versions")
         local("tar -czvf versions/web_static_{}.tgz web_static/"
