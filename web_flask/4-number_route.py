b35ca4dab947eb8c5d860476c7f5fb83b278d361
mehdizit <zitounimehdi.it@gmail.com>
2021-05-01 02:42:46 +0100

    up

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+""" starts a Flask web application"""
+
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_holberton():
+    """Displays 'Hello HBNB!'"""
+    return '“Hello HBNB!”'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """display hbnb"""
+    return "“HBNB”"
+
+
+@app.route('/c/<string:text>', strict_slashes=False)
+def c_is_fun(text):
+    """displaty c is fun"""
+    return "C %s" % text.replace('_', ' ')
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<string:text>', strict_slashes=False)
+def python_is_cool(text="is cool"):
+    """display Python is ..."""
+    return "Python %s" % text.replace('_', ' ')
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def num(n):
+    """display only integer"""
+    return '%i is a number' % n
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
