5cf0bd6b832bcc1c76a69340a0f257c46b73c135
mehdizit <zitounimehdi.it@gmail.com>
2021-05-02 01:37:03 +0100

    up

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -2,8 +2,7 @@
 """ starts a Flask web application"""
 
 
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
@@ -38,11 +37,11 @@ def num(n):
     return '%i is a number' % n
 
 
-@app.route('/number_template/<int=n>', strict_slashes=False)
+@app.route('/number_template/<int:n>', strict_slashes=False)
 def number_temp(n):
     """display a HTML page only if n is an integer"""
     return render_template('5-number.html', n=n)
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0')
